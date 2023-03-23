72bae84f9c9d8450f5b882f86c440a569bcd13d9
Maxon Harris <maxonharris@Maxons-Air.attlocal.net>
2023-03-23 10:06:16 -0500

    ITDEV 160 Project

---
+++ js.script.js
@@ -0,0 +1,7 @@
+var message = 'Welcome';
+var messageTwo = message + '! Here Is a Brief Overview Of Goal Explosions';
+var messageThree = document.getElementById('messageTwo');
+var information = 'Information';
+var info = document.getElementById('information');
+info.textContent = information
+messageThree.textContent = messageTwo;
