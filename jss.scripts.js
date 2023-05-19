9d36c6ef29accd251570260c0f24761c80b68456
Maxn-H <harrm136@gmatc.matc.edu>
2023-05-19 14:54:31 -0500

    ITDEV-PROJECT 1 Pushed

--- jss.scripts.js
+++
@@ -1,8 +0,0 @@
-var message = 'Welcome';
-var messageTwo = message + '! Here Is a Brief Overview Of Goal Explosions';
-var messageThree = document.getElementById('messageTwo');
-var information = 'Information';
-var info = document.getElementById('information');
-info.textContent = information
-messageThree.textContent = messageTwo;
-
